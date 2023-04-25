30db0177b3b64f04352a5bfe8fd4f5146598802d
chizk0 <jose.amador.salazar@est.una.ac.cr>
2023-04-24 19:06:47 -0600

    [ADD redux]
    se agrego la carpeta root junto con los componentes hasta el paso 6

--- gif-shop-app/src/pages/_app.tsx
+++ gif-shop-app/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import  "@/styles/globals.css";
+import  type  { AppProps }  from  "next/app";
+import  { Provider }  from  "react-redux";
+import  ApplicationStore  from  "@/root/redux/store";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export  default  function  App({  Component,  pageProps  }:  AppProps)  {
+	return (
+		<Provider  store={ApplicationStore}>
+			<Component  {...pageProps}  />
+		</Provider>
+	);
 }
+
---
+++ gif-shop-app/src/root/redux/thunks/gif-thunk/gif.thunk.ts
@@ -0,0 +1,12 @@
+import { gifProvider } from "../../providers/gif-provider/gif.provider";
+import { DispatchType, setGifs } from "../../reducers/gif-reducer/gifReducer";
+
+
+export const startSetGif = (searchTerm: string): any => {
+  return async (dispatch: DispatchType) => {
+    const gifList = await gifProvider(searchTerm);
+
+    dispatch(setGifs(gifList || []));
+  };
+};
+
